abc2481e8f3c228c734d1b1afe3f45ffbdc1c533
Sidor4ik <isidorenya.lenovo@gmail.com>
2024-01-31 00:47:36 +0100

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,10 +1,15 @@
-const create = async () => {
-	let text = "I am fresh and young";
-	
-	fs.writeFile("fresh.txt", text, (Eror) => {
-		if (Eror)
-		  console.log('FS operation failed ');
-	 });
-};
+import fs from "node:fs";
+import path from "node:path"
 
+const create = async () => {
+const fileText = "I am fresh and young";
+const fileName = "fresh.txt";
+const dirPath = "src/fs/files";
+
+const filePath = path.join(dirPath, fileName);
+
+fs.writeFile(filePath, fileText, { flag: 'wx' }, (err) => {
+   if(err){console.error('FS operation failed')}
+  })
+}
 await create();
