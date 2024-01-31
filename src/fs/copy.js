8f6093e5e2e55074c66a1cbb79b4991bef3b84d7
Sidor4ik <isidorenya.lenovo@gmail.com>
2024-01-31 02:20:14 +0100

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,22 @@
-const fs = require('fs');
+import fs from "node:fs/promises";
+import path from "node:path";
 
 const copy = async () => {
+	const sourcePath = path.resolve("src/fs/files");
+	const destinationPath = path.resolve("src/fs/files_copy");
+ 
+	try {
+	  await fs.cp(sourcePath, destinationPath, {
+		 force: false,
+		 recursive: true,
+		 errorOnExist: true,
+	  });
+	} catch (err) {
+	  if (err.code === "ENOENT" || err.code === "ERR_FS_CP_EEXIST") {
+		 throw new Error("FS operation failed");
+	  }
+	}
+
 };
 
 await copy();
