371652e0d0515e7ce46cce1355804e2e5c447548
Sidor4ik <isidorenya.lenovo@gmail.com>
2024-01-31 17:18:25 +0100

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const sourcePath = path.resolve("src/fs/files/wrongFilename.txt");
+const destinationPath = path.resolve("src/fs/files/properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+   const sourcePath = path.resolve("src/fs/files/wrongFilename.txt");
+  const targetFile = path.resolve("src/fs/files/properFilename.md");
+  try {
+    await fs.access(targetFile, fs.constants.F_OK);
+
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+    try {
+      await fs.rename(sourcePath, destinationPath);
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        throw new Error("FS operation failed");
+      } else {
+        throw err;
+      }
+    }
+  }
 };
 
 await rename();
