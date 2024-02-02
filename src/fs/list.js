1dfd493eae388348299645df69b037159dd46bf2
Sidor4ik <isidorenya.lenovo@gmail.com>
2024-02-02 16:18:25 +0100

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const list = async () => {
-    // Write your code here 
+	const dirName = path.resolve("src/fs/files")
+
+	try {
+	  const dirContents = await fs.readdir(dirName)
+	 for (const file of dirContents) {
+		 console.log(file);
+	  };
+	} catch (err) {
+	  if (err.code === "ENOENT") {
+		 throw new Error("FS operation failed")
+	  } else {
+		 throw err;
+	  }
+	}
 };
 
 await list();
